8c53a59a5f6fdfc77550383b574008f3679e4e34
Flávio Vinícius Martins dos Santos <vinicius0809@gmail.com>
2021-09-04 11:22:21 -0300

    First commit

---
+++ functions/index.js
@@ -0,0 +1,87 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+// const https = require('https');
+admin.initializeApp();
+const regionFunctions = functions
+    .runWith({ timeoutSeconds: 60, memory: "128MB" })
+    .region("southamerica-east1").https;
+
+exports.updateParticipants = regionFunctions.onCall(
+    async (data) => {
+        functions.logger.info(data);
+        if (data) {
+            let jsObject = data;
+            let message = "Houve erro ao realizar transação!";
+            jsObject.insertDate = admin.firestore.FieldValue.serverTimestamp();
+            const participantsRef = admin.firestore().collection("participants");
+            await participantsRef
+                .add(jsObject)
+                .then(async () => {
+                    await participantsRef.orderBy("insertDate", "asc")
+                        .limit(1)
+                        .get()
+                        .then((querySnapshot) => {
+                            querySnapshot.forEach((doc) => {
+                                participantsRef.doc(doc.id).delete()
+                                    .then(() => {
+                                        message = "Participantes atualizados e mais antigo excluído!";
+                                    })
+                            });
+                        })
+
+                })
+                .catch((error) => {
+                    functions.logger.info(error.message);
+                    throw new functions.https.HttpsError('unknown', error.message, error);
+                })
+                .finally(() => {
+                    functions.logger.info(message);
+                    return message;
+                })
+        }
+        else {
+            const msg = "Erro ao receber JSON!";
+            functions.logger.info(msg);
+            return msg;
+        }
+    });
+
+exports.getParticipants = regionFunctions.onCall(async (data) => {
+    const participantsRef = admin.firestore().collection("participants");
+
+    
+});
+
+// exports.updatePage = regionFunctions.onCall(async (data, context) => {
+//     const baseURI = String.toString(data);
+
+//     return new Promise((resolve, reject) => {
+//         const request = https.get((baseURI, (res) => {
+//             res.on('data', (d) => {
+//                 functions.logger.info("Teste do logs!");
+//                 functions.logger.info(d);
+//                 return "funciona!"
+//             });
+//             res.on('end', resolve);
+//         }));
+//         request.on('error', reject);
+//     });
+// });
+
+// exports.test = regionFunctions.onRequest((request, response) => {
+//     const baseURI = 'https://api.itau/open-banking/products-services/v1/personal-accounts'
+//     this.$http.get(baseURI)
+//         .then((result) => {
+//             functions.logger.info("Teste do logs!");
+//             functions.logger.info(result.data);
+//             response.send(result.data);
+//         })
+// })
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
---
+++ src/firebase/app.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+// import {initializeApp } from 'firebase/app';
+import 'firebase/functions';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+// import 'firebase/storage';
+
+const config = {
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+};
+console.log(config);
+
+export const App = firebase.initializeApp(config);
+
+// export const AUTH = App.auth();
+// export const DB = App.firestore();
+// export const STORAGE = App.storage();
+export const FUNCTIONS = App.functions("southamerica-east1");
